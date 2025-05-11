56d2046f5370781206a438c0f6dc40a6b6171dd6
Ajeet Singh Raina, Docker Captain, ARM Innovator <ajeetraina@gmail.com>
2025-05-11 19:16:20 +0530

    Add App.js for KubeTools Portal

---
+++ portal/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import KubeToolsPortal from './KubeToolsPortal';
+
+function App() {
+  return (
+    <div className="App">
+      <KubeToolsPortal />
+    </div>
+  );
+}
+
+export default App;
